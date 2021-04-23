21d25fc446637590743c86419bb4232b99af32a2
khaled <khaledsardouk@gmail.com>
2021-04-23 13:16:00 +0300

    software app changes

--- gui-test.py
+++ gui-test.py
@@ -1,28 +1,56 @@
 from tkinter import *
+import time
+window = Tk()
+window.title("form")
+window.geometry("800x300")
 import pymongo
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-mydb = myclient['practice']
-mycol = mydb["names"]
-
-def insertToDb(_fname,_lname):
-        if mycol.count_documents({ "first_name": _fname, "last_name": _lname  }) >0:
-                print("user already exist ")
-        else :
+def insertToDb(_fname,_lname,window,listbox,col):
+      label2_txt=""
+      Label2 = Label(window, text=label2_txt)  
+      if mycol.count_documents({ "first_name": _fname, "last_name": _lname  }) >0:
+              Label2['text']="user already exist"
+              Label2.grid (row=4,column=1)
+                
+      else :
                 mydict = { "first_name": _fname, "last_name": _lname }
                 x= mycol.insert_one(mydict)
-                print(f'first name is {_fname} and last name is {_lname} added to the database ')
+                Label2['text']="added to the database"
+                Label2.grid (row=4,column=1)
+                print(f' {_fname}  {_lname} added to the database ')
+                printNames(listbox,col)
+def printNames(listbox1,collection):
+        for x in collection.find():
+                listbox1.insert(0,x)
+       
+try: 
+        myclient = pymongo.MongoClient('mongodb://localhost:27017/',serverSelectionTimeoutMS = 2000)
+        myclient.server_info()
+        mydb = myclient['practice']
+        mycol = mydb["names"]    
+               
+        Label1 = Label(window, text='First name:').grid(row=0, column=0)
+        Fname = Entry(window)
+        Fname.grid(row=0, column=1)
+        Label1 = Label(window, text='Last name:').grid(row=1, column=0)
+        lname = Entry(window)
+        lname.grid(row=1, column=1)
+        listbox = Listbox(window,height = 10,width =80)
+        listbox.place(relx=0.3, rely=0.1)
+        printNames(listbox,mycol) 
+        button1 = Button(window, text="submit", command=lambda: insertToDb(Fname.get(), lname.get(),window,listbox,mycol)).place(relx=0.4, rely=0.8, anchor=CENTER)     
+        button1 = Button(window, text="exit",fg='red', command=window.destroy).place(relx=0.6, rely=0.8, anchor=CENTER)
+        window.mainloop()
 
 
-window = Tk()
+except:
+        Label1 = Label(window, text='connection error please connect to the server').place(relx=0.5, rely=0.2, anchor=CENTER)
+        button1 = Button(window, text="exit",fg='red', command=window.destroy).place(relx=0.5, rely=0.5, anchor=CENTER)
+        window.mainloop()
+        
 
-window.title("form")
-window.geometry("300x200")
-Label1 = Label(window, text='First name:').grid(row=0, column=0)
-Fname = Entry(window)
-Fname.grid(row=0, column=1)
-Label1 = Label(window, text='Last name:').grid(row=1, column=0)
-lname = Entry(window)
-lname.grid(row=1, column=1)
-button1 = Button(window, text="submit", command=lambda: insertToDb(Fname.get(), lname.get())).grid(row=2)
 
-window.mainloop()
+
+
+
+
+
