02228cffb078fe1b28e933500f12389fcb654f25
hadisalloum01 <72878894+hadisalloum01@users.noreply.github.com>
2021-05-29 13:21:48 +0300

    test

--- Main/cogs/database.py
+++ Main/cogs/database.py
@@ -43,7 +43,7 @@ class database(commands.Cog):
 
     @commands.command(pass_context=True)
     async def GetTimezones(self,ctx):
-        return await ctx.send(file=discord.File(r'./timezones.txt'))
+        return await ctx.send(file=discord.File(r'../../timezones.txt'))
 
 
     @commands.command(pass_context=True)
